ffec14c2fee64864cbfe77f0e03204e63df1270d
low-key <lokesh@low-key.me>
2025-12-15 07:18:52 +0530

    initial mobile setup

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,25 +1,7 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = {
-      "jmbuhr/cmp-pandoc-references",
-      "mikavilpas/blink-ripgrep.nvim",
-    },
     opts = {
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer", "ripgrep", "references" },
-        providers = {
-          lsp = { fallbacks = {} },
-          ripgrep = {
-            module = "blink-ripgrep",
-            name = "ripgrep",
-          },
-          references = {
-            name = "pandoc_references",
-            module = "cmp-pandoc-references.blink",
-          },
-        },
-      },
       keymap = {
         preset = "default",
       },
