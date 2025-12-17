422f447bbe60f93ea73f9226dc46574dcd9c9a27
low-key <lokesh@low-key.me>
2025-12-17 19:14:37 +0530

    add personal snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,23 +1,10 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = {
-      "jmbuhr/cmp-pandoc-references",
-      "mikavilpas/blink-ripgrep.nvim",
-    },
     opts = {
       sources = {
-        default = { "lsp", "path", "snippets", "buffer", "ripgrep", "references" },
         providers = {
           lsp = { fallbacks = {} },
-          ripgrep = {
-            module = "blink-ripgrep",
-            name = "ripgrep",
-          },
-          references = {
-            name = "pandoc_references",
-            module = "cmp-pandoc-references.blink",
-          },
         },
       },
       keymap = {
